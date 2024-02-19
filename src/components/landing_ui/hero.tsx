b1c24cddea695f39b87b2ece5a3b604b7bbcfdc2
muzammil-15 <muzammilcoder15@gmail.com>
2024-02-19 18:15:12 +0500

    changes

--- src/components/landing_ui/hero.tsx
+++ src/components/landing_ui/hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
           </p>
 
 
-          <Button link="/work" title="Components"/>
+          <Button link="/components" title="Components"/>
         </div>
       </div>
   );
